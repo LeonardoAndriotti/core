4c10f9297645f6e74796e09adc0b0b5d8a247f9d
LeonardoAndriotti <leonardoandriotte@hotmail.com>
2017-09-30 01:05:44 -0300

    initial commit

---
+++ src/main/java/com/core/ProjectPrincipalApplication.java
@@ -0,0 +1,12 @@
+package com.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPrincipalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPrincipalApplication.class, args);
+	}
+}
