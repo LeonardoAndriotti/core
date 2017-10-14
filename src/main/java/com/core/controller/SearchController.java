a0f388184961eeb4cd92ad5f8902f907dc80fc25
LeonardoAndriotti <leonardoandriotte@hotmail.com>
2017-10-14 00:54:32 -0300

    tela pesquisar produto

--- src/main/java/com/core/controller/SearchController.java
+++ src/main/java/com/core/controller/SearchController.java
@@ -3,28 +3,31 @@ package com.core.controller;
 import com.core.DTO.ProductDTO;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.util.List;
 
+@RestController
+@RequestMapping(value = "/api")
 public class SearchController {
 
+    @GetMapping(value = "/{name}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<ProductDTO> searchProduct(@PathVariable String product) throws RestClientException, IOException {
 
-    public void getEmployee() throws RestClientException, IOException {
+        String url = "https://api-product.herokuapp.com/product/" + product;
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<List<ProductDTO>> rateResponse =
-                restTemplate.exchange("https://api-product.herokuapp.com/product",
+        ResponseEntity<List<ProductDTO>> response =
+                restTemplate.exchange(url,
                         HttpMethod.GET, null, new ParameterizedTypeReference<List<ProductDTO>>() {
                         });
-        List<ProductDTO> product = rateResponse.getBody();
-        System.out.println(product.get(0).getName());
-    }
-
-
-    public static void main(String[] args) throws IOException {
-        new SearchController().getEmployee();
+         return response.getBody();
     }
 }
