3c01a8c813c9301547569cf010833e1a9492d83f
RenatoSoutoMaior <fr6aswlu>
2017-10-22 22:04:50 -0200

    Alterações no Cadastro de Produtos

--- src/main/resources/static/js/controller/cadastro-produto-controller.js
+++ src/main/resources/static/js/controller/cadastro-produto-controller.js
@@ -1,19 +1,9 @@
-youBuyApp.controller("cadastroProdutoController", function($scope, $http){
+angular.module('youBuyApp',[]).controller('cadastroProdutoController', function($scope) {
 
-    $scope.produtos = [];
+    $scope.produto = {};
 
-    carregarCadastroProduto = function(){
-        $http({method:'GET', url: 'http://localhost:8092/product'})
-        .then(function(response){
-            $scope.produtos = response.data;
-
-        }, function(response){
-            console.log(response.data);
-            console.log(response.status);
-
-        });
-
+    $scope.cadastrarProduto = function(produto){
+        $scope.produto.nome = $scope.nome;
+        console.log($scope.produto.nome);
     };
-
-    carregarCadastroProduto();
 });
