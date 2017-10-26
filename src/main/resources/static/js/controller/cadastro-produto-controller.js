fe630500da4cd8241fe1f80d7c6b40d697373315
renato.souto <renato.souto@matera.com>
2017-10-26 10:45:14 -0200

    Refatorando controller para teste

--- src/main/resources/static/js/controller/cadastro-produto-controller.js
+++ src/main/resources/static/js/controller/cadastro-produto-controller.js
@@ -1,18 +1,3 @@
-// youBuyApp.controller('cadastroProdutoController', function ($scope, $http) {
-//
-//     $scope.produtos = {};
-//
-//     cadastrarProduto = function () {
-//         $http({method: 'POST', url: 'http://localhost:8092/product'})
-//             .then(function (response) {
-//                 console.log("Deu bom!");
-//
-//             }, function (response) {
-//                 console.log("Deu ruim!");
-//             });
-//     };
-// });
-
 (function () {
     'use strict';
 
@@ -28,7 +13,6 @@
 
         vm.cadastrarProduto = cadastrarProduto;
         vm.category1 = {id:1};
-        vm.category2 = {id:2};
 
         function cadastrarProduto() {
             vm.product.category = vm.category1;
