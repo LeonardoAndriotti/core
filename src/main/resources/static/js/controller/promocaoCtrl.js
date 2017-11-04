b392e3b5e54da3a487c87976c0a0d9eaae292761
Edgar <edgarponte@gmail.com>
2017-11-04 17:35:33 -0200

    Configurando ordenacao do itens

--- src/main/resources/static/js/controller/promocaoCtrl.js
+++ src/main/resources/static/js/controller/promocaoCtrl.js
@@ -3,7 +3,7 @@ youBuyApp.controller("promocaoController", function($scope, $http){
     $scope.produtos = [];
     $scope.myDecimal = 0;
     $scope.selected = [];
-
+    /*Função que faz o GET de produtos na api-produto*/
     carregarProdutos = function(){
         $http({method:'GET', url: 'https://api-product.herokuapp.com/product'})
         .then(function(response){
@@ -17,14 +17,16 @@ youBuyApp.controller("promocaoController", function($scope, $http){
         });
 
     },
-    
-        $scope.ordenar = function(keyname){
-            $scope.sortKey = keyname;
-            $scope.reverse = !$scope.reverse;
+    /*Ordenar a lista para tela */
+    $scope.ordenar = function(){
+        langKey = $scope.selected;
+        $scope.sortKey = langKey;
+        /*$scope.reverse = !$scope.reverse;*/
     },
-    
+    /*Adiciona os itens na lista do checkbox*/
     $scope.toggle = function(item, list){
         var idx = list.indexOf(item);
+
         if (idx > -1) {
             list.splice(idx, 1);
             console.log(list);
@@ -34,7 +36,7 @@ youBuyApp.controller("promocaoController", function($scope, $http){
         }
 
     },
-
+    /*Verifica se existe itens na lista*/
     $scope.exists = function (item, list){
         return list.indexOf(item) > -1;
     };
