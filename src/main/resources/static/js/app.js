fda7ce404d9059368615ff039be6aa1dcd5e38d2
LeonardoAndriotti <leonardoandriotte@hotmail.com>
2017-11-06 19:02:24 -0200

    lista de compras,pagina html

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,21 +2,25 @@ var youBuyApp = angular.module("youBuyApp", ['ngRoute']);
 
 youBuyApp.config(function ($routeProvider, $locationProvider) {
     $routeProvider
-    .when("/search", {
-        templateUrl: 'views/search.html',
-        controller: 'searchController'
-    })
-    .when("/promocao", {
-        templateUrl: 'views/promocao.html',
-        controller: 'promocaoController'
-    })
-    .when("/cadastro-produto", {
-        templateUrl: 'views/cadastro-produto.html',
-        controller: 'cadastroProdutoController as vm'
-    })
-    .otherwise({
-        rediretTo: '/'
-    });
+        .when("/search", {
+            templateUrl: 'views/search.html',
+            controller: 'searchController'
+        })
+        .when("/promocao", {
+            templateUrl: 'views/promocao.html',
+            controller: 'promocaoController'
+        })
+        .when("/cadastro-produto", {
+            templateUrl: 'views/cadastro-produto.html',
+            controller: 'cadastroProdutoController as vm'
+        })
+        .when("/shoppingList", {
+            templateUrl: 'views/shoppingList.html',
+            controller: 'shoppingListController'
+        })
+        .otherwise({
+            rediretTo: '/'
+        });
 
     $locationProvider.hashPrefix('').html5Mode(true);
 });
