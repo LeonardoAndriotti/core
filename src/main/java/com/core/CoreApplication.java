8fb47f53ca456c7f4965eebb4711ebff714ae5b5
RicardoLuizSilveira <ricardo.silveira@gmail.com>
2017-11-13 21:18:47 -0200

    Criando HashMap de mercados

--- src/main/java/com/core/CoreApplication.java
+++ src/main/java/com/core/CoreApplication.java
@@ -1,10 +1,16 @@
 package com.core;
 
+import com.core.DTO.ShopDTO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.List;
+
 @SpringBootApplication
 public class CoreApplication {
+
+	public static HashMap<String, List<ShopDTO>> ShopGrid;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
