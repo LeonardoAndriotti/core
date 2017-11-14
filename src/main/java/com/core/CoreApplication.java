6b334d5b9d27ee79e12ae7828b5a6b86f6630371
RicardoLuizSilveira <ricardo.silveira@gmail.com>
2017-11-14 21:49:02 -0200

    Movendo Map estático de mercados

--- src/main/java/com/core/CoreApplication.java
+++ src/main/java/com/core/CoreApplication.java
@@ -1,18 +1,20 @@
 package com.core;
 
-import com.core.DTO.ShopDTO;
+import com.core.service.Geo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.HashMap;
-import java.util.List;
 
 @SpringBootApplication
 public class CoreApplication {
 
-	public static HashMap<String, List<ShopDTO>> ShopGrid;
+
 
 	public static void main(String[] args) {
+
+		Geo.shopGrid = new HashMap<>();
+
 		SpringApplication.run(CoreApplication.class, args);
 	}
 }
