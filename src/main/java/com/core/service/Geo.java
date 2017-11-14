f1128e19423a18973e1e62819ba93de38ce01940
RicardoLuizSilveira <ricardo.silveira@gmail.com>
2017-11-14 21:51:33 -0200

    Adicionando Map estático e adicionando estrutura de decisão de busca

--- src/main/java/com/core/service/Geo.java
+++ src/main/java/com/core/service/Geo.java
@@ -1,5 +1,37 @@
 package com.core.service;
 
+import com.core.DTO.ShopDTO;
+import com.core.controller.GeoController;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class Geo {
 
+    public static HashMap<String, List<ShopDTO>> shopGrid;
+
+    public List<ShopDTO> getContainShopList(String key){
+        if (!shopGrid.containsKey(key)){
+            shopGrid.put(key, getShopDTOS(key));
+        }
+        return shopGrid.get(key);
+    }
+
+    private List<ShopDTO> getShopDTOS(@RequestParam("gridCode") String gridCode) {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<List<ShopDTO>> responseEntity =
+                restTemplate.exchange("https://api-user.herokuapp.com/product",
+                        HttpMethod.GET, null, new ParameterizedTypeReference<List<ShopDTO>>() {
+                        });
+        List<ShopDTO> shopDTOList = responseEntity.getBody();
+        shopGrid.put(gridCode, shopDTOList);
+        return shopDTOList;
+    }
+
 }
+
