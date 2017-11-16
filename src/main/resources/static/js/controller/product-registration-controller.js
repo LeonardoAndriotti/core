31bcfdbc0f8bfabde8b8c88d682ef110da03478e
RenatoSoutoMaior <fr6aswlu>
2017-11-16 08:58:05 -0200

    Alterações no Registro de Produtos

--- src/main/resources/static/js/controller/product-registration-controller.js
+++ src/main/resources/static/js/controller/product-registration-controller.js
@@ -5,18 +5,18 @@
         .module('youBuyApp')
         .controller('productRegistrationController', ProductRegistrationController);
 
-    ProductRegistrationController.$inject = ['$http'];
+    ProductRegistrationController.$inject = ['$scope', '$http', '$route'];
 
-    function ProductRegistrationController($http) {
+    function ProductRegistrationController($scope, $http, $route) {
 
         var vm = this;
-
         vm.registerProduct = registerProduct;
 
         function registerProduct() {
             $http.post('http://localhost:8092/product', vm.product)
                 .then(function (response) {
                     console.log("Deu bom!");
+                    $route.reload();
                 }, function (response) {
                     console.log("Deu ruim! " + JSON.stringify(response));
                 });
