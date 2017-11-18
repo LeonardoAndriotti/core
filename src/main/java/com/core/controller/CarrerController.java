0d4be33ad403f0ef9c0f20dd6e9622d43b022433
Raul Maia <raul.mysterious1@gmail.com>
2017-11-18 10:52:38 -0200

    Atualizando carrer

--- src/main/java/com/core/controller/CarrerController.java
+++ src/main/java/com/core/controller/CarrerController.java
@@ -8,19 +8,28 @@ import java.util.List;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.core.DTO.CarrerDTO;
 import com.core.DTO.ProductDTO;
 
-
+@RestController
 public class CarrerController {
 	
+//	@RequestMapping(value ="/carrer", method= RequestMethod.GET)
+//	public @ResponseBody List<ProductDTO> TodosProdutos() throws RestClientException, IOException{
+//		return getAllProduct(1);
+//	}
 	
-	public List<ProductDTO> getAllProduct(int id) throws RestClientException, IOException{
-		
-		List<CarrerDTO> carrer = getCarrer(id);
+	@RequestMapping(value ="/carrer/{iduser}", method= RequestMethod.GET)
+	public List<ProductDTO> getAllProduct(@PathVariable BigDecimal iduser) throws RestClientException, IOException{
+		List<CarrerDTO> carrer = getCarrer(iduser);
 		List<ProductDTO> products = new ArrayList<ProductDTO>();
 		for (int i = 0; i < carrer.size(); i++) {
 			System.out.println(carrer.get(i).getIdproduto());
@@ -33,10 +42,10 @@ public class CarrerController {
 		return products;
 	}
 	
-	 public List<CarrerDTO> getCarrer(int id) throws RestClientException, IOException {
+	 public List<CarrerDTO> getCarrer(BigDecimal iduser) throws RestClientException, IOException {
 		 RestTemplate restTemplate = new RestTemplate();
 			 ResponseEntity<List<CarrerDTO>> rateResponse =
-		                restTemplate.exchange("http://localhost:8091/carrer/iduser/"+id,
+		                restTemplate.exchange("http://localhost:8091/carrer/iduser/"+iduser,
 		                        HttpMethod.GET, null, new ParameterizedTypeReference<List<CarrerDTO>>() {
 		                        });
 			 	List<CarrerDTO> carrer = rateResponse.getBody();
@@ -54,8 +63,8 @@ public class CarrerController {
 	    }
 
 
-	    public static void main(String[] args) throws IOException {
-	        new CarrerController().getAllProduct(1);
-	    }
+//	    public static void main(String[] args) throws IOException {
+//	        new CarrerController().getAllProduct(1);
+//	    }
 
 }
--- src/main/resources/static/js/controller/carrerController.js
+++ src/main/resources/static/js/controller/carrerController.js
@@ -1,29 +1,17 @@
 youBuyApp.controller("carrerController", function($scope, $http){
 
-    $scope.carrer = [];
-    $scope.value = ['14.50','18.20','22.49'];
-    $scope.quant = 1;
-    
-    $scope.getTotal = function(){
-        var soma = 0;
-        for (var i = 0; i < $scope.value.length; i++) {
-			soma += ($scope.value[i] * $scope.quant);
-		}
-        return soma;
-    }
-    
-//    carregarCarrer = function(){
-//        $http({method:'GET', url: 'http://localhost:8093/order'})
-//        .then(function(response){
-//            $scope.carrer = response.data;
-//
-//        }, function(response){
-//            console.log(response.data);
-//            console.log(response.status);
-//
-//        });
-//
-//    };
-//
-//    carregarCarrer();
+    carregarCarrer = function(){
+        $http({method:'GET', url: 'http://localhost:8900/carrer/1'})
+        .then(function(response){
+            $scope.carrer = response.data;
+
+        }, function(response){
+            console.log(response.data);
+            console.log(response.status);
+
+        });
+
+    };
+
+    carregarCarrer();
 });
