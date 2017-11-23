a1b15392166d0a1c6513158282f28578689e91c1
renato.souto <renato.souto@matera.com>
2017-11-23 12:04:01 -0200

    Corringindo rotas

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,31 +1,31 @@
-var youBuyApp = angular.module("youBuyApp", ['ngRoute']);
+var youBuyApp = angular.module("youBuyApp", ['ngRoute', 'ngAnimate', 'ngSanitize', 'ui.bootstrap']);
 
 youBuyApp.config(function ($routeProvider, $locationProvider) {
     $routeProvider
         .when("/", {
-		templateUrl : 'views/search.html',
-		controller : 'searchController'
-	}).when("/timeline/order/:id", {
-		templateUrl : 'views/order.html',
-		controller : 'orderController'
-	}).when("/list/order", {
-		templateUrl : 'views/list-order.html',
-		controller : 'listOrderController'
-	}).when("/promocao", {
-		templateUrl : 'views/promocao.html',
-		controller : 'promocaoController'
-	}).when("/product-registration", {
-		templateUrl : 'views/product-registration.html',
-		controller : 'productRegistrationController as vm'
-	}).when("/shoppingList", {
-		templateUrl : 'views/shoppingList.html',
-		controller : 'shoppingListController as vm'
-	}).when("/carrer", {
-		templateUrl : 'views/carrer.html',
-		controller : 'carrerController'
-	}).otherwise({
-		rediretTo : '/'
-	});
+            templateUrl: 'views/search.html',
+            controller: 'searchController'
+        }).when("/timeline/order/:id", {
+        templateUrl: 'views/order.html',
+        controller: 'orderController'
+    }).when("/list/order", {
+        templateUrl: 'views/list-order.html',
+        controller: 'listOrderController'
+    }).when("/promocao", {
+        templateUrl: 'views/promocao.html',
+        controller: 'promocaoController'
+    }).when("/product-registration", {
+        templateUrl: 'views/product-registration.html',
+        controller: 'productRegistrationController as vm'
+    }).when("/shoppingList", {
+        templateUrl: 'views/shoppingList.html',
+        controller: 'shoppingListController as vm'
+    }).when("/carrer", {
+        templateUrl: 'views/carrer.html',
+        controller: 'carrerController'
+    }).otherwise({
+        rediretTo: '/'
+    });
 
     $locationProvider.hashPrefix('').html5Mode(true);
 });
