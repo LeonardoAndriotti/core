752fb8ac68731f0d1ff173708cba57a7c5271f6c
LeonardoAndriotti <leonardoandriotte@hotmail.com>
2017-11-25 02:01:55 -0200

    tela de pesquisa salvando no carrinho de compra, menu drop, maquina de status de pedidos

--- src/main/resources/static/js/controller/searchController.js
+++ src/main/resources/static/js/controller/searchController.js
@@ -15,12 +15,10 @@ youBuyApp.controller('searchController', function ($scope, $http) {
         });
     };
     $scope.addCart = function (id,quant,value) {
-      //  var teste = parse(value.substring(2));
         var money = value.replace("R$","").replace(".",",");
         $http.get('http://localhost:8900/api/product/save/carrier/'+id +'/'+quant+'/'+money).then(function (response) {
             $scope.products = convertMoney(response);
         });
-      alert(id +" "+ quant + " " + value.substring(2));
     };
 
     $scope.search = function () {
